84cf30cbfdaf3320db3102325bd042b3f7c40db6
mengjianhua <mengjianhua@bilibili.com>
2023-08-03 18:53:31 +0800

    icon AND title

--- fe/vue.config.js
+++ fe/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: './../electron_gui/dist'
+  outputDir: './../electron_gui/dist',
+  chainWebpack(config){
+    config.plugin('html').tap(args => {
+      args[0].title = 'Lan同步'
+      return args
+    })
+  }
 })
