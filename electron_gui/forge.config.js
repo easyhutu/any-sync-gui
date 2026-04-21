3602cad84a3154a1c92292e69f5f6e79b18f1c0a
mengjianhua <mengjianhua@bilibili.com>
2026-04-21 16:23:28 +0800

    up

--- electron_gui/forge.config.js
+++ electron_gui/forge.config.js
@@ -14,6 +14,12 @@ module.exports = {
       config: {},
     },
     {
+      name: '@electron-forge/maker-dmg',
+      config: {
+        format: 'ULFO'
+      },
+    },
+    {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
     },
